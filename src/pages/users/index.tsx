37c1e04348d637e8c99592f435530d25df277801
Joao Ferreira Neto <joao.neto@AWSDEV.CORP>
2021-04-13 13:53:58 -0300

    Fake de dados com Mirage

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,9 +12,12 @@ import {
   Tbody,
   Td,
   Checkbox,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner,
+  toLocaleDateString
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useQuery } from "react-query";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -22,6 +25,27 @@ import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
+
+  const {data, isLoading, error} = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json();
+    
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      };
+    });
+
+    return users;
+  });
+
   const isWideVersion = useBreakpointValue ({
         base: false,
         lg: true,
@@ -51,105 +75,66 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Joao Neto</Text>
-                    <Text fontSize="small" color="gray.300">
-                      joao@hhotmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td> 04 de abril 2021</Td>}
-                {isWideVersion && 
-                  <Td>
-                      <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine}  fontSize="18"/>}
-                          >
-                          Editar
-                      </Button>
-                  </Td>
-                }
-              </Tr>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obeter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => {
+                    return(
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="small" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        {isWideVersion && 
+                          <Td>
+                              <Button
+                                  as="a"
+                                  size="sm"
+                                  fontSize="sm"
+                                  colorScheme="purple"
+                                  leftIcon={<Icon as={RiPencilLine}  fontSize="18"/>}
+                                  >
+                                  Editar
+                              </Button>
+                          </Td>
+                        }
+                      </Tr>
+                    )
+                  })}
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Joao Neto</Text>
-                    <Text fontSize="small" color="gray.300">
-                      joao@hhotmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td> 04 de abril 2021</Td>}
-                {isWideVersion && 
-                  <Td>
-                      <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine}  fontSize="18"/>}
-                          >
-                          Editar
-                      </Button>
-                  </Td>  
-                }
-              </Tr>
+                </Tbody>
+              </Table> 
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Joao Neto</Text>
-                    <Text fontSize="small" color="gray.300">
-                      joao@hhotmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td> 04 de abril 2021</Td>}
-                {isWideVersion && 
-                  <Td>
-                      <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine}  fontSize="18"/>}
-                          >
-                          Editar
-                      </Button>
-                  </Td>
-                }
-              </Tr>
-            </Tbody>
-          </Table> 
-
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
